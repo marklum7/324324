8313fcf5b58cd08b493948c197e470c0bfabdff8
user1 <user1@PC4435>
2024-02-26 13:21:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/FileName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public abstract class Animal
+    {
+        public abstract string pol { get; set; }
+    }
+
+    public class Pet : Animal
+    {
+        public override string pol { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Cat : Pet
+    {
+        public string color { get; set; }
+    }
+
+    public class Dog : Pet
+    {
+        public string poroda { get; set; }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp2;
+using System.Xml.Linq;
+
+Cat  cat = new Cat{pol="ломинат", name = "керыч", color = "black"};
+
+Dog dog = new Dog { pol = "паркет", name = "велера", poroda = "баландино" };
+
+Pet dog2 = (Pet)dog;
+
+object cat2 = cat as Object;
+Pet cat3 = (Pet)cat2;
